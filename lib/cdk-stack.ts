f03f48172ca3b37ef2e1b6faff1f101e69bc35d5
rojiwon0325 <bond9986@naver.com>
2022-04-16 15:19:16 +0900

    init

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,16 +1,33 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { aws_ecs, aws_ecs_patterns, Stack, StackProps } from "aws-cdk-lib";
+import { Vpc } from "aws-cdk-lib/aws-ec2";
+import { Cluster } from "aws-cdk-lib/aws-ecs";
+import { Construct } from "constructs";
 
 export class CdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new Vpc(this, "rojiwon-cdk-vpc", { maxAzs: 2 });
+    const cluster = new Cluster(this, "rojiwon-cdk-ecs", { vpc });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const LoadBalancedFargateService =
+      new aws_ecs_patterns.ApplicationLoadBalancedFargateService(
+        this,
+        "rojiwon-cdd-service",
+        {
+          cluster,
+          desiredCount: 2,
+          memoryLimitMiB: 1024,
+          taskImageOptions: {
+            image: aws_ecs.ContainerImage.fromRegistry(
+              "amazon/amazon-ecs-sample"
+            ),
+            environment: {
+              // TEST_ENVIRONMENT_VARIABLE1: "test environment variable 1 value",
+              // TEST_ENVIRONMENT_VARIABLE2: "test environment variable 2 value",
+            },
+          },
+        }
+      );
   }
 }
