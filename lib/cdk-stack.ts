f300d66191c2067ffc431720184347069a8f80cf
rojiwon0325 <bond9986@naver.com>
2022-04-16 18:36:12 +0900

    fin

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,4 +1,11 @@
-import { aws_ecs, aws_ecs_patterns, Stack, StackProps } from "aws-cdk-lib";
+import "dotenv/config";
+import {
+  aws_ecs,
+  aws_ecs_patterns,
+  aws_route53,
+  Stack,
+  StackProps,
+} from "aws-cdk-lib";
 import { Vpc } from "aws-cdk-lib/aws-ec2";
 import { Cluster } from "aws-cdk-lib/aws-ecs";
 import { Construct } from "constructs";
@@ -7,27 +14,37 @@ export class CdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    const config = {
+      Domain: process.env.DOMAIN as string,
+      SubDomain: `${process.env.SUB_OF_DOMAIN}.${process.env.DOMAIN}`,
+    };
+
     const vpc = new Vpc(this, "rojiwon-cdk-vpc", { maxAzs: 2 });
     const cluster = new Cluster(this, "rojiwon-cdk-ecs", { vpc });
+    const hostzone = aws_route53.HostedZone.fromLookup(this, "SubZone", {
+      domainName: config.Domain,
+    });
 
-    const LoadBalancedFargateService =
-      new aws_ecs_patterns.ApplicationLoadBalancedFargateService(
-        this,
-        "rojiwon-cdk-service",
-        {
-          cluster,
-          desiredCount: 2,
-          memoryLimitMiB: 1024,
-          taskImageOptions: {
-            image: aws_ecs.ContainerImage.fromRegistry(
-              "amazon/amazon-ecs-sample"
-            ),
-            environment: {
-              // TEST_ENVIRONMENT_VARIABLE1: "test environment variable 1 value",
-              // TEST_ENVIRONMENT_VARIABLE2: "test environment variable 2 value",
-            },
+    // const LoadBalancedFargateService =
+    new aws_ecs_patterns.ApplicationLoadBalancedFargateService(
+      this,
+      "rojiwon-cdk-service",
+      {
+        domainZone: hostzone,
+        domainName: config.SubDomain,
+        cluster,
+        desiredCount: 3,
+        memoryLimitMiB: 1024,
+        taskImageOptions: {
+          image: aws_ecs.ContainerImage.fromRegistry(
+            "amazon/amazon-ecs-sample"
+          ),
+          environment: {
+            // TEST_ENVIRONMENT_VARIABLE1: "test environment variable 1 value",
+            // TEST_ENVIRONMENT_VARIABLE2: "test environment variable 2 value",
           },
-        }
-      );
+        },
+      }
+    );
   }
 }
